7eb06fc81f5a17baa6f39b9fca11148c1cb60a61
landiluigi746 <landiluigi865@gmail.com>
2025-08-01 11:37:45 +0200

    [fix]: Fix missing sudo handling in config on Linux

--- src/Config.hpp
+++ src/Config.hpp
@@ -7,24 +7,26 @@
 namespace ggpkg::Config
 {
 #if defined(_WIN32)
-    inline constexpr std::string_view CFG_PATH_ENV_VAR = "LOCALAPPDATA";
+    inline const std::filesystem::path BASE_CONFIG_PATH = Utils::GetEnv("LOCALAPPDATA");
 #elif defined(__linux__)
-    inline constexpr std::string_view CFG_PATH_ENV_VAR = "HOME";
+    inline const std::string SUDO_USER = Utils::GetEnv("SUDO_USER");
+    inline const std::filesystem::path BASE_CONFIG_PATH =
+        (SUDO_USER.empty()) ? std::filesystem::path(Utils::GetEnv("HOME"))
+                            : std::filesystem::path("/home") / SUDO_USER;
 #else
 #error "Unsupported platform"
 #endif
 
-    inline const std::filesystem::path HOME_PATH = Utils::GetEnv(CFG_PATH_ENV_VAR);
-
 #if defined(_WIN32)
     inline const std::filesystem::path PACKAGE_MANAGER_CONFIG_PATH =
-        HOME_PATH / "ggpkg" / "package-manager.json";
-    inline const std::filesystem::path PACKAGES_CONFIG_PATH = HOME_PATH / "ggpkg" / "packages.json";
+        BASE_CONFIG_PATH / "ggpkg" / "package-manager.json";
+    inline const std::filesystem::path PACKAGES_CONFIG_PATH =
+        BASE_CONFIG_PATH / "ggpkg" / "packages.json";
 #elif defined(__linux__)
     inline const std::filesystem::path PACKAGE_MANAGER_CONFIG_PATH =
-        HOME_PATH / ".config" / "ggpkg" / "package-manager.json";
+        BASE_CONFIG_PATH / ".config" / "ggpkg" / "package-manager.json";
     inline const std::filesystem::path PACKAGES_CONFIG_PATH =
-        HOME_PATH / ".config" / "ggpkg" / "packages.json";
+        BASE_CONFIG_PATH / ".config" / "ggpkg" / "packages.json";
 #else
 #error "Unsupported platform"
 #endif
